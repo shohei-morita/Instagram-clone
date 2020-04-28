13851f002ede0740cd8684da154f14c6fe95251f
shohei morita <shomorita1985@gmail.com>
2020-04-28 14:26:00 +0900

    ログイン画面作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+
+    else
+      flash.now[:danger] = %q(ログインに失敗しました)
+      render :new
+    end
+  end
 end
