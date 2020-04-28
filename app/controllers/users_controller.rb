4ab9449d106ad92cd0a0da68f84248586ec99075
shohei morita <shomorita1985@gmail.com>
2020-04-28 09:13:54 +0900

    Userモデルのnameとemailのバリデーション設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
