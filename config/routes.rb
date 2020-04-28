c6f70de9d4f9da264182bf21b5542909d890f633
shohei morita <shomorita1985@gmail.com>
2020-04-29 00:46:31 +0900

    picturesコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'users#new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :pictures
 end
