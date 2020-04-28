12dd3451d71cdf4c34ef41d59281afc473844d20
shohei morita <shomorita1985@gmail.com>
2020-04-29 08:39:21 +0900

    共有画面の作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,24 @@
 class PicturesController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(picture_params)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:title, :content, :image)
   end
 end
