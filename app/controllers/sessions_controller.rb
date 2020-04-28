5b78bfaf7931413edf590ec792c1679ac81ba73a
shohei morita <shomorita1985@gmail.com>
2020-04-28 15:03:46 +0900

    ログイン・ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:sessions][:email].downcase)
     if user && user.authenticate(params[:sessions][:password])
-
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = %q(ログインに失敗しました)
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = %q(ログアウトしました)
+    redirect_to new_session_path
   end
 end
