d9d94cc8db97ef36f1d6186b966f140cb852e6d3
shohei morita <shomorita1985@gmail.com>
2020-04-30 10:16:26 +0900

    お気に入り登録・解除機能修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    favorite = current_user.favorites.create(picture_id: params[:id])
-    flash[:notice] = %Q({favorite.picture.user.name}さんの投稿をお気に入りに登録しました)
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    flash[:notice] = %Q(#{favorite.picture.user.name}さんの投稿をお気に入りに登録しました)
     redirect_to pictures_path
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    flash[:notice] = %Q({favorite.picture.user.name}さんの投稿をお気に入り解除しました)
+    flash[:notice] = %Q(#{favorite.picture.user.name}さんの投稿をお気に入り解除しました)
     redirect_to pictures_path
   end
 end
