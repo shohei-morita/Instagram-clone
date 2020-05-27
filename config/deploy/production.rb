d7fadb97754490cf574017b698f0d71969a8a28f
shohei morita <shomorita1985@gmail.com>
2020-05-27 23:58:29 +0900

    編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '54.250.131.167', user:'app', roles:%w{app db web}
-set :ssh_options, keys:'/usr/local/ssh/id_rsa'
+server '54.250.131.167', user: 'app', roles: %w{app db web}
+set :ssh_options, keys:'/Users/apple/.ssh/id_rsa'
